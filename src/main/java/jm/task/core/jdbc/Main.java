8857435c0608451a427a38dbad3329c37e062efb
Andrey198o <voenkov.java.dev@gmail.com>
2024-03-27 14:07:14 +0300

    В классах Main и UserServiceImpl изменил объявление объектов на UserDao

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,25 +1,32 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class Main {
     public static void main(String[] args) {
 
-        UserService userService = new UserServiceImpl();
+        UserDao UserDao = new UserDaoJDBCImpl();
 
-        userService.createUsersTable();
-        userService.createUsersTable();
-        userService.saveUser("Name1", "LastName1", (byte) 20);
-        userService.saveUser("Name2", "LastName2", (byte) 25);
-        userService.saveUser("Name3", "LastName3", (byte) 31);
-        userService.saveUser("Name4", "LastName4", (byte) 38);
-        userService.getAllUsers();
-        //userService.removeUserById(4);
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+        UserDao.createUsersTable();
+
+        UserDao.saveUser("Name1", "LastName1", (byte) 20);
+        UserDao.saveUser("Name2", "LastName2", (byte) 25);
+        UserDao.saveUser("Name3", "LastName3", (byte) 31);
+        UserDao.saveUser("Name4", "LastName4", (byte) 38);
+
+        UserDao.removeUserById(4);
+        UserDao.getAllUsers();
+        UserDao.cleanUsersTable();
+        UserDao.dropUsersTable();
         Util.closeConnection();
+//        Util.closeSessionFactory();
+
 
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,34 +1,43 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    UserDao  UserDao  = new UserDaoJDBCImpl();
+//    UserDao  UserDao  = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
-        userDaoJDBC.createUsersTable();
+        UserDao.createUsersTable();
+//        UserDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userDaoJDBC.dropUsersTable();
+        UserDao.dropUsersTable();
+//        UserDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        userDaoJDBC.saveUser(name, lastName, age);
+        UserDao.saveUser(name, lastName, age);
+//        UserDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        userDaoJDBC.removeUserById(id);
+        UserDao.removeUserById(id);
+//        UserDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return userDaoJDBC.getAllUsers();
+        return UserDao.getAllUsers();
+//        return UserDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userDaoJDBC.cleanUsersTable();
+        UserDao.cleanUsersTable();
+//        UserDao.cleanUsersTable();
     }
 }
